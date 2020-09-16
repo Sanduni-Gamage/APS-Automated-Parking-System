bd0eb442d788097c5d32d7e3b62f3b2dd6e0607c
Gamitha <gamithamanawadu26@gmail.com>
2020-09-16 17:42:22 +0530

    first commit

--- app/src/main/java/com/example/aps/DatabaseHelper.java
+++ app/src/main/java/com/example/aps/DatabaseHelper.java
@@ -14,27 +14,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("Create table User(email text primary key, name text, mobile text, nic text, address text, password text)");
-        db.execSQL("Create table Feedback(email text primary key, name text, message text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists User");
-        db.execSQL("drop table if exists Feedback");
-    }
-
-    public Boolean insert(String name, String email, String mobile, String nic, String address, String password){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name",name);
-        contentValues.put("email",email);
-        contentValues.put("mobile",mobile);
-        contentValues.put("nic",nic);
-        contentValues.put("address",address);
-        contentValues.put("password",password);
-        long ins = db.insert("User", null, contentValues);
-        if(ins==-1) return false;
-        else return true;
     }
 
     public Boolean update(String name, String email, String mobile, String nic, String address) {
@@ -88,28 +72,4 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    public Boolean insert(String name, String email, String message){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name",name);
-        contentValues.put("email",email);
-        contentValues.put("message",message);
-        long ins = db.insert("Feedback", null, contentValues);
-        if(ins==-1) return false;
-        else return true;
-    }
-
-    public Boolean chkemail(String email){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("Select * from User where email=?",new String[]{email});
-        if(cursor.getCount()>0) return false;
-        else return true;
-    }
-
-    public Boolean emailpassword(String email, String password){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("Select * from User where email=? and password=?",new String[] {email,password});
-        if(cursor.getCount()>0) return true;
-        else return false;
-    }
 }
